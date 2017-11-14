47a43c1839df9ad28e6f3e4b6e71dd6ee48c442f
Joseph Osako <josephosako@gmail.com>
2017-11-14 13:06:47 -0500

    Second step of the demo evaluator, and added simple REPL function.
    Also added the utility function "atom?".

--- basic-evaluator/basic-eval.scm
+++ basic-evaluator/basic-eval.scm
@@ -1,3 +1,22 @@
 (define basic:eval
   (lambda (expr env)
-    ))
+    (if (atom? expr)
+        
+        (case (car expr)
+          (else (error "EVAL - could not evaluate list"))))))
+
+  
+(define atom?
+  (lambda (expr)
+    (not (pair? expr))))
+  
+(define basic:repl
+  (lambda ()
+    (display "Basic evaluator test REPL")
+    (let loop ((lexical-env '((parent ()))))
+      (display #\newline)
+      (display #\newline)
+      (display ">>> ")
+      (let ((result (basic:eval (read) env)))
+        (display result)
+        (loop env)))))
