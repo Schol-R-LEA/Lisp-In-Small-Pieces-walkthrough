04f365a41765a9d256bc8b6d5908df913d165279
Joseph Osako <josephosako@gmail.com>
2017-11-17 23:45:14 -0500

    Snapshot: added basic environment handling functions and invoke (a minimal form of 'apply' used for this example).

--- basic-evaluator/basic-eval.scm
+++ basic-evaluator/basic-eval.scm
@@ -15,15 +15,22 @@
           ((set!) (update! (cadr expr) env (basic:eval (caddr expr) env)))
           ((lambda) (make-function (cadr expr) (cddr expr) env))
           (else (invoke (basic:eval (cdr expr) env)
-                             (evlis (cdr expr) env)))))))
+                        (evlis (cdr expr) env)))))))
+
+(define invoke
+  (lambda (fn args)
+    (if (procedure? fn)
+        (fn args)
+        (error "not a function: " fn))))
+
 
 ;;; utility functions
-  
+
 (define atom?
   (lambda (expr)
     (not (pair? expr))))
 
-(define empty-result '*<unspecified>*)
+(define empty-result '*unspecified*)
 
 (define eprogn
   (lambda (exprs env)
@@ -35,18 +42,67 @@
             (basic:eval (car exprs) env))
         empty-result)))
 
+
 (define evlis
+  (lambda (exprs env)
   (if (pair? exprs)
-      (cons (basic:eval (car exps) env)
-            evlis (cdr exps) env)
-      '()))
+      (cons (basic:eval (car exprs) env)
+            evlis (cdr exprs) env)
+      '())))
+
+
+(define hash
+  (lambda (key)        ;; stub for possible future need
+    key))
+
+
+(define valid?
+  (lambda (value)
+    (not (null? value))))
+
+(define parent-env '**parent**)
+
+(define lookup
+  (lambda (key env)
+    (let ((value (assq (hash key) env))
+          (parent (assq parent-env env)))
+      (cond ((valid? value)
+             value)
+            ((valid? parent)
+             (lookup key parent))
+            (else (error "LOOKUP - no value bound to symbol " key))))))
+
+(define update!
+  (lambda (key env value)
+    (let ((variable (lookup key env)))
+      (if (valid? variable)
+          (begin
+            (set-cdr! variable value)
+            empty-result)))))
+
+
+(define make-env (env '()))
+
+(define extend-env
+  (lambda (env variables values)
+    (cond ((pair? variables)
+           (if (pair? values)
+               (cons (cons (car variables) (car values))
+                     (extend-env env (cdr variables) (cdr values)))
+               (error "Too few values"))
+           ((null? variables)
+            (if (null? values)
+                env
+                (error "Too few variables")))
+           ((symbol? variables)
+            (cons (cons variables values) env))))))
 
 ;;; simple REPL
 
 (define basic:repl
   (lambda ()
     (display "Basic evaluator test REPL")
-    (let loop ((env '((parent ()))))
+    (let loop ((make-env))
       (display #\newline)
       (display #\newline)
       (display ">>> ")
